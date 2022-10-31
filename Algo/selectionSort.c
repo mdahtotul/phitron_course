225ac47cb9c3ea357f4c2b50baa4d3262d5a758a
arifulhasantotul <mdahtotul@gmail.com>
2022-11-01 01:57:31 +0600

    done

---
+++ Algo/selectionSort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int a[] = {1, 3, 9, 8, 4, 6, 7, 3};
+  int N = sizeof(a) / sizeof(a[0]);
+  int i, temp;
+
+  temp = a[0]
+  for (i = 0, i < N; i++){
+    if(a[0] > a[i]){
+      
+    }
+  }
+
+  return 0;
+}
