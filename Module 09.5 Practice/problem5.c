326d87c043549415a44f423e14e9a798220b94d9
arifulhasantotul <mdahtotul@gmail.com>
2022-11-01 14:43:59 +0600

    problem 5 solved

--- Module 09.5 Practice/problem5.c
+++ Module 09.5 Practice/problem5.c
@@ -2,6 +2,29 @@
 
 int main()
 {
-  int N, arr[N], i, j;
+  int N, M, arr[N], i, out=0;
+  printf("Enter total size of array: ");
+  scanf("%d", &N);
+  printf("Enter %d elements in the array: ", N);
+  for ( i = 0; i < N; i++)
+  {
+    scanf("%d ", &arr[i]);
+  }
+  
+  scanf("%d", &M);
+
+  for (i = 0; i < N-1; i++){
+    if((arr[i] + arr[i+1]) == M){
+      out = 1;
+      break;
+    }
+  }
+
+  if(out == 0){
+    printf("NO");
+  }else {
+    printf("YES");
+  }
+
   return 0;
 }
