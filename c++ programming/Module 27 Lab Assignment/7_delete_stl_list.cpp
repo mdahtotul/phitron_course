deb08eb27ae351c19904073038a8608a3676e013
arifulhasantotul <mdahtotul@gmail.com>
2023-02-10 18:15:06 +0600

    exam done

--- c++ programming/Module 27 Lab Assignment/7_delete_stl_list.cpp
+++ c++ programming/Module 27 Lab Assignment/7_delete_stl_list.cpp
@@ -10,19 +10,17 @@ void printList( list<int> l ) {
    cout<<"\n";
 }
 
-void deleteValue( list<int>&l,list<int>cl, int value ) {
-   auto it = cl.begin();
+void deleteValue( list<int>&l, int value ) {
+   auto it = l.begin();
    int idx = 0;
-   while( it != cl.end() ) {
+   while( it != l.end() ) {
       if( *it == value )
+      {
+         l.erase( it );
          break;
+      }
       it++;
-      idx++;
    }
-
-   auto ite = l.begin();
-   advance( ite, idx );
-   l.erase( ite );
 }
 
 int main() {
@@ -35,7 +33,7 @@ int main() {
    l.push_front( 3 );
    l.push_front( 7 );
    printList( l );
-   deleteValue( l,l, 4 );
+   deleteValue( l, 4 );
    printList( l );
    return 0;
 }
